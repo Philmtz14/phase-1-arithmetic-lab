42e4c44a9d903d351ae016e0dd86fa4f255cbc86
philip martinez <martinez.philip00@gmail.com>
2022-03-04 05:15:15 -0600

    commit tree ed4dc71ef9726d5dcc4790f29682894429c01929

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 
 // Write your code here
+const num1 = 2
+const num2 = 31
+
+let multiply = num1 * num2;
+
+const random = Math.floor(Math.random() * 10) + 1;
+
+const num3 = 7
+const num4 = 3
+
+let mod = num3 - num4;
+
+let max = Math.max(5,10,15,20);
